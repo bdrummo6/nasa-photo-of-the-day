1b4dbd935d6e22a591a0a56b4bfe5623ef0e265f
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-03-07 18:58:41 -0500

    Project Part 1 Submission
    MVP met

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
+import Card from './components/Card/Card';
+import Heading from './components/Heading/Heading';
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
+   const [data, setData] = useState([]);
+
+   useEffect(() => {
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=z1ai1KAnu9Yv9igrdGPzZJHSn5023TLvvrpCmIjA`)
+         .then(response => {
+            console.log(response.data);
+            setData(response.data);
+
+         })
+         .catch(err => {
+            console.log(`Error: ${err}`);
+         })
+   }, []);
+
+
+   return (
+      <div className="app">
+         <Heading/>
+         <Card explanation={data.explanation} date={data.date} url={data.url}/>
+      </div>
+   );
 }
 
 export default App;
